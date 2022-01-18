85718394c19f4251dcfecbd73979adc6542c9a36
Ahn-Gihwan <ahngh0113@naver.com>
2022-01-18 17:21:26 +0900

    feat: setting for Use react-query

--- react-query/src/App.jsx
+++ react-query/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import Datas from "./components/Datas";
+
+const queryClient = new QueryClient();
 
 export const App = () => {
-  return <div className="app">React</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Datas />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
